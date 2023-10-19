767699918060814f69c201d46e025a8cdbba99dd
playsamay4 <70341772+playsamay4@users.noreply.github.com>
2023-10-18 18:57:29 -0700

    purple -> red

--- Transition.lua
+++ Transition.lua
@@ -1,5 +1,5 @@
 Transition = {
-    PurplePart = {
+    RedPart = {
         x = 0,
         y = 1080+500,
         w = 1920,
@@ -17,7 +17,7 @@ Transition = {
 
 function DrawTransition()
     love.graphics.setColor(Transition.col[1],Transition.col[2],Transition.col[3],0.95)
-    love.graphics.rectangle("fill",Transition.PurplePart.x,Transition.PurplePart.y,Transition.PurplePart.w,Transition.PurplePart.h)
+    love.graphics.rectangle("fill",Transition.RedPart.x,Transition.RedPart.y,Transition.RedPart.w,Transition.RedPart.h)
 
     love.graphics.setColor(0.8,0.8,0.8,1)
     love.graphics.rectangle("fill",Transition.WhitePart.x,Transition.WhitePart.y,Transition.WhitePart.w,Transition.WhitePart.h)
@@ -25,12 +25,12 @@ end
 
 
 function ShowTransition()
-    if PurpleAnim then PurpleAnim:stop() end
+    if RedAnim then RedAnim:stop() end
     if WhiteAnim then WhiteAnim:stop() end
 
-    Transition.PurplePart.y = 1080+500
+    Transition.RedPart.y = 1080+500
     Transition.WhitePart.y = 1080+1100
 
-    PurpleAnim = Anim.to(Transition.PurplePart,1.3,{y = -500})
+    RedAnim = Anim.to(Transition.RedPart,1.3,{y = -500})
     WhiteAnim = Anim.to(Transition.WhitePart,1.3,{y = -1100}):delay(0.1)
 end
